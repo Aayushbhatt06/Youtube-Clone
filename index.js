3378a9f9270d329d53f9b2c04523676d71be18f2
Aayush Bhatt <152008288+Aayushbhatt06@users.noreply.github.com>
2025-07-27 18:57:04 +0530

    !st Iteration Complete

--- index.js
+++ index.js
@@ -19,14 +19,13 @@ await mongoose.connect('mongodb://localhost:27017/yt-clone');
 let results;
 app.get('/', async (req, res) => {
   results = await Video.find({});
-  res.render('index', { results }); // pass to index.ejs
+  res.render('index', { results });
 });
 app.get('/video/:slug', async (req, res) => {
   try {
     const slug = req.params.slug;
     const video = await Video.findById(slug);
     console.log(video);
-    // Fetch other videos to show on right sidebar
     const results = await Video.find({ _id: { $ne: slug } }).limit(10);
 
     res.render('video_page', { video, results });
@@ -40,31 +39,3 @@ app.get('/video/:slug', async (req, res) => {
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-
-// let result;
-// async function fetchData() {
-//   await mongoose.connect('mongodb://localhost:27017/yt-clone');
-//   result = await MyModel.find({});
-//   await mongoose.disconnect(); 
-// }
-
-// let container = document.querySelector(".r-content");
-
-// results.map((video) => {
-//   let card = document.createElement("div");
-//   card.className = "vid-item";
-//   card.innerHTML = `
-//   <div class="item w-[390px] cursor-pointer">
-//     <img class="w-full h-[210px] object-cover rounded-xl" src="${video.thumb}" alt="">
-//     <div class="flex gap-3 mt-3">
-//       <img class="w-9 h-9 rounded-full" src="${video.ch_logo}" alt="">
-//       <div>
-//         <p class="text-sm font-semibold leading-snug mb-1">${video.title}</p>
-//         <p class="text-xs text-gray-400 mb-0.5">${video.channel}</p>
-//         <p class="text-xs text-gray-400">${video.views} views • ${video.release} ago</p>
-//       </div>
-//     </div>
-//   </div>`;
-//   container.appendChild(card);
-// });
